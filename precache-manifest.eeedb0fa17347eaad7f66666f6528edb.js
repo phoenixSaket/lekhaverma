eb049b1dd9bbec34ffc9eae0d45be0af7e63c61c
Saket Verma <saketverma@plexusmd.com>
2020-07-07 18:04:19 +0530

    Updates

---
+++ precache-manifest.eeedb0fa17347eaad7f66666f6528edb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4becf1fa5c8c66a2e430ef70f8f04251",
+    "url": "/lekhaverma/index.html"
+  },
+  {
+    "revision": "1a1162b87882bf339584",
+    "url": "/lekhaverma/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4b0dbb867db7cc5a6562",
+    "url": "/lekhaverma/static/css/main.7dae3a6a.chunk.css"
+  },
+  {
+    "revision": "1a1162b87882bf339584",
+    "url": "/lekhaverma/static/js/2.403d80f5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/lekhaverma/static/js/2.403d80f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b0dbb867db7cc5a6562",
+    "url": "/lekhaverma/static/js/main.8202af3c.chunk.js"
+  },
+  {
+    "revision": "1d44ca812e627e388471",
+    "url": "/lekhaverma/static/js/runtime-main.7a8dbed3.js"
+  },
+  {
+    "revision": "f8d6bb0504accf149055cbb28b61f206",
+    "url": "/lekhaverma/static/media/BG.f8d6bb05.png"
+  },
+  {
+    "revision": "38c49bc46487cbcb875c13a8cadf8f31",
+    "url": "/lekhaverma/static/media/lekhaAvatar.38c49bc4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lekhaverma/precache-manifest.5d5ebb0857bade63c497495eb134eefc.js"
+  "/lekhaverma/precache-manifest.eeedb0fa17347eaad7f66666f6528edb.js"
 );
 
 self.addEventListener('message', (event) => {
